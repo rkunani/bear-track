02c09f69f094049b16e0bb8582992e85870f05ca
Raguvir Kunani <rkunani@berkeley.edu>
2020-09-07 18:06:17 -0700

    autofill dashes in phone #

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -12,11 +12,22 @@ export class LoginComponent {
 
   constructor(private authService: AuthService, private router: Router) { }
 
+  validatePhoneNo(field) {
+    var phoneNumDigits = field.value.replace(/\D/g, '');
+
+    var formattedNumber = phoneNumDigits;
+    if (phoneNumDigits.length >= 6)
+      formattedNumber = phoneNumDigits.substring(0, 3) + '-' + phoneNumDigits.substring(3, 6) + '-' + phoneNumDigits.substring(6);
+    else if (phoneNumDigits.length >= 3)
+      formattedNumber = phoneNumDigits.substring(0, 3) + '-' + phoneNumDigits.substring(3);
+
+    field.value = formattedNumber;
+  }
+
   onLogin(form: NgForm) {
     if (form.invalid) {
       return;
     }
-    // this.isLoading = true;
     this.authService.login(form.value.name, form.value.phone);
   }
 

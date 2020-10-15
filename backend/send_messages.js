69d356b61f770b03712fe8368f0698be64a7b9bb
Raguvir Kunani <rkunani@berkeley.edu>
2020-10-15 16:43:50 -0700

    add a link to app to reactivate the track after user is notified

--- backend/send_messages.js
+++ backend/send_messages.js
@@ -2,12 +2,12 @@ const Track = require('./models/track');
 const User = require('./models/user');
 const axios = require('axios');
 const util = require('util');
-const { time } = require('console');
 
 const accountSid = process.env.TWILIO_ACCT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const twilioClient = require('twilio')(accountSid, authToken);
 const twilioNumber = process.env.TWILIO_NUMBER;
+const APP_URL = "tinyurl.com/bear-track-app";
 
 function isRequestedStatus(currentEnrollment, status) {
   const isOpen = (currentEnrollment.enrolled < currentEnrollment.enrolled_max);
@@ -18,7 +18,7 @@ function isRequestedStatus(currentEnrollment, status) {
   return false;
 }
 
-// Approach #1: Iterate through all tracks, querying enrollment API each time
+/* Approach #1: Iterate through all tracks, querying enrollment API each time */
 module.exports = () => {
   Track.find()
     .then( (tracks) => {
@@ -31,38 +31,38 @@ module.exports = () => {
             const enrollmentHistory = response.data.data;
             const currentEnrollment = enrollmentHistory[enrollmentHistory.length - 1];
             if (isRequestedStatus(currentEnrollment, track.status)) {
-              // find the creator
+              /* Notify the user, if they haven't already been notified */
               User.findOne({ _id: track.creator })
                 .then( (user) => {
-                  if (!track.notified) {  // only send text once per track
-                    const message = util.format("From BearTrack:\nHi %s, %s is %s!", user.name, track.course_code, track.status);
-                    console.log(message);
-                    // send a message
+                  if (!track.notified) {
+                    const message = util.format(
+                      "From BearTrack:\nHi %s, %s is %s!\n\nIf you'd like to be notified again, visit %s/tracks/list to reactivate the track.",
+                      user.name, track.course_code, track.status, APP_URL);
                     twilioClient.messages.create({
                       body: message,
                       from: twilioNumber,
                       to: user.phone
                     }).then(message => console.log("Sent message: " + message.sid))
                       .catch( (error) => { console.log("Could not send message"); });
-                    // mark the track as notified
+                    /* Mark the track as notified */
                     track.notified = true;
                     Track.updateOne({ _id: track._id }, track)
-                      .then( (result) => { console.log("Updated track to 'notified': true"); });
+                      .then( (result) => {});
                   }
                 })
                 .catch( (error) => { console.log("could not find creator"); });
             }
           })
           .catch( (error) => {
-            // weird situation where request has status code 500 but enrollment data is still returned,
-            // so the request enters both the then and catch blocks
-            // (theoretically) unable to get enrollment info for this class
+            /* Weird situation where request has status code 500 but enrollment data is still returned,
+            so the request enters both the then and catch blocks
+            (theoretically) unable to get enrollment info for this class */
             console.log("GET request to " + requestURL + " failed!");
           });
       }
     })
     .catch( (error) => {
-      // unable to get tracks
+      /* Unable to get tracks */
       console.log("unable to fetch tracks from DB");
     });
 }
